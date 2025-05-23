3961cca88c630b987e2bd650b8a6949de0ebda3e
Nic Simms <nsimms@asu.edu>
2025-05-22 21:44:04 -0700

    outline and more mean

--- eda_tools.py
+++ eda_tools.py
@@ -24,10 +24,43 @@ def my_mean(dataframe):
     
     return sum / count 
 
+def my_std_stat(dataframe):
+    """
+    This is my own creation for std, this will not be useful over pandas or numpy.
+    This is for statistics practice in python.
+
+    Args:
+        dataframe (DataFrame): Pandas DataFrame 
+    
+    Returns:
+        std (float): std statistic for the dataframe input.
+    """
+
+
+
+#TODO: 
+#2. Outlier Detection
+#    Z-score based
+#    IQR based
+
+#TODO: 
+#3. Correlation Matrix with Visual
+
+#TODO:
+#4. Pairplot & Distributions
+#    Use sns.pairplot()
+#    sns.histplot() or sns.kdeplot() for each numerical feature
+
+#Nice-to-Have Extras
+#    Profiling report export (like pandas-profiling or ydata-profiling)
+#    Categorical variable summaries (value counts, mode, entropy)
+#    Save plots to files or PDF report
+
+#Import dataset
 crop_recommend_df = pd.read_csv(filepath_or_buffer="datasets/Crop_recommendation.csv")
 #print(df.head())
 
-test_df = crop_recommend_df[["rainfall"]]
+test_df = crop_recommend_df.drop(["label"], axis = 1)
 #print(test_df.head())
 
 def get_summary_stats(dataframe):
@@ -40,5 +73,6 @@ def get_summary_stats(dataframe):
 #print(get_summary_stats(crop_recommend_df.drop("label", axis = 1)))
 
 #Test my fucntion against the Pandas mean() fucntion and compare if the results match.
-print("My mean function: ", my_mean(test_df))
-print("Pandas built in mean function: ", test_df.mean())
+print("My mean function: \n", my_mean(test_df))
+print("Pandas built in mean function: \n", test_df.mean())
+#Results match the builtin Pandas function.
--- test.py
+++ test.py
@@ -11,11 +11,22 @@ print(sklearn.__version__)
 crop_recommend_df = pd.read_csv(filepath_or_buffer="datasets/Crop_recommendation.csv")
 #print(df.head())
 
-test_df = crop_recommend_df[["rainfall"]]
+test_df = crop_recommend_df.drop(["label"], axis = 1)
+
+test_df_1_col = test_df["rainfall"]
 #print(test_df.head())
 
 df_shape = test_df.shape
-print(df_shape)
-print(type(df_shape))
-print(df_shape[0])
-print(df_shape[1])
+#print(df_shape)
+#print(type(df_shape))
+#print(df_shape[0])
+#print(df_shape[1])
+
+count = 0
+sum = 0
+for row in range(df_shape[0]):
+    count += 1
+    sum += test_df_1_col.loc[row]
+
+test_df_mean = sum / count
+
