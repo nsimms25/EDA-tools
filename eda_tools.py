d8047c1d74d3c682850da19c9bdca3c77670e6e4
Nic Simms <nsimms@asu.edu>
2025-05-23 04:16:18 +0000

    Implement my mean and compared to pandas builtin

---
+++ eda_tools.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+import scipy.stats
+
+def my_mean(dataframe):
+    """
+    This is my own creation for mean, this will not be useful over pandas or numpy.
+    This is for statistics practice in python.
+
+    Args:
+        dataframe (DataFrame): Pandas DataFrame 
+    
+    Returns:
+        Mean (int): mean statistic for the dataframe input.
+    """
+    df_shape = dataframe.shape
+    row_count = df_shape[0]
+    column_count = df_shape[1]
+    count = 0 
+    sum = 0
+    for i in range(row_count):
+        count += 1
+        sum += dataframe.loc[i]
+    
+    return sum / count 
+
+crop_recommend_df = pd.read_csv(filepath_or_buffer="datasets/Crop_recommendation.csv")
+#print(df.head())
+
+test_df = crop_recommend_df[["rainfall"]]
+#print(test_df.head())
+
+def get_summary_stats(dataframe):
+    return dataframe.describe().T.assign(
+        missing_pct=dataframe.isnull().mean() * 100,
+        skewness=dataframe.skew(),
+        kurtosis=dataframe.kurt()
+    )
+
+#print(get_summary_stats(crop_recommend_df.drop("label", axis = 1)))
+
+#Test my fucntion against the Pandas mean() fucntion and compare if the results match.
+print("My mean function: ", my_mean(test_df))
+print("Pandas built in mean function: ", test_df.mean())
--- test.py
+++ test.py
@@ -7,3 +7,15 @@ print(np.__version__)
 print(pd.__version__)
 print(scipy.__version__)
 print(sklearn.__version__)
+
+crop_recommend_df = pd.read_csv(filepath_or_buffer="datasets/Crop_recommendation.csv")
+#print(df.head())
+
+test_df = crop_recommend_df[["rainfall"]]
+#print(test_df.head())
+
+df_shape = test_df.shape
+print(df_shape)
+print(type(df_shape))
+print(df_shape[0])
+print(df_shape[1])
